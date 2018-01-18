809d3e2490fff2749c5c4de6587101dc71a36efe
Johnny Miller <johnnysviva@outlook.com>
2018-01-18 15:54:50 +0800

    Import module: MyOkHttp

--- app/src/main/java/com/jm/newvistabeta/MainActivity.java
+++ app/src/main/java/com/jm/newvistabeta/MainActivity.java
@@ -2,12 +2,19 @@ package com.jm.newvistabeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickLogin(View view) {
+
     }
 }
