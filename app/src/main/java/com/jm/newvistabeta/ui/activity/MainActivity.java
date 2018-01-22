be431e71b20bec046eec9e8d70a0a8c1894d2d81
Johnny Miller <johnnysviva@outlook.com>
2018-01-22 17:49:14 +0800

    UI enhancement

--- app/src/main/java/com/jm/newvistabeta/ui/activity/MainActivity.java
+++ app/src/main/java/com/jm/newvistabeta/ui/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.jm.newvistabeta.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.jm.newvistabeta.R;
 
